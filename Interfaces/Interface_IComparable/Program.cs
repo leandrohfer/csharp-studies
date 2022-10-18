b72abe2a553133b3148205cc87e1369ea1ca7b27
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-10-18 15:12:50 -0300

    Implementação da IComparable
    Exemplo de uso de uma comparação de objetos pré-definida, utilizando o compareTo da interface IComparable.

---
+++ Interfaces/Interface_IComparable/Program.cs
@@ -0,0 +1,30 @@
+﻿using Interface_IComparable.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"C:\laragon\www\csharp-studies\Interfaces\Interface_IComparable\test.txt";
+
+		try
+		{
+			using (StreamReader sr = File.OpenText(path))
+			{
+				List<Employee> list = new List<Employee>();
+				while (!sr.EndOfStream)
+				{
+					list.Add(new Employee(sr.ReadLine()));
+				}
+				list.Sort();
+				foreach (Employee emp in list)
+				{
+					Console.WriteLine(emp);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			Console.WriteLine("An error occurred " + e.Message);
+		}
+    }
+}
