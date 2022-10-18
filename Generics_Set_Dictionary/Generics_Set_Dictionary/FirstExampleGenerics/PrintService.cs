88726d4920214275f3bbe1860baca99e424cae89
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-10-18 20:10:38 -0300

    Exemplo de Restrição de Generics

--- Generics_Set_Dictionary/Generics_Set_Dictionary/FirstExampleGenerics/PrintService.cs
+++ Generics_Set_Dictionary/Generics_Set_Dictionary/FirstExampleGenerics/PrintService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Generics_Set_Dictionary
+namespace Generics_Set_Dictionary.FirstExampleGenerics
 {
     // Exemplo de uso do Generic
     class PrintService<T>
---
+++ Generics_Set_Dictionary/Generics_Set_Dictionary/FirstExampleGenerics/ProgramOld.cs
@@ -0,0 +1,24 @@
+﻿using Generics_Set_Dictionary.FirstExampleGenerics;
+
+namespace Generics_Set_Dictionary.FirstExampleGenerics
+{
+    class ProgramOld1
+    {
+        static void Teste(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
