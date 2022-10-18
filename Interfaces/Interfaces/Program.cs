a52651628f0ad554bb6d3590295cbb258e3ed1bf
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-10-18 01:44:15 -0300

    Resolução de Exercício COM o uso de Interfaces
    Trata-se de uma correção do commit anterior implementando o uso de interface usando inversão de controle por meio de injeção de dependência.

--- Interfaces/Interfaces/Program.cs
+++ Interfaces/Interfaces/Program.cs
@@ -24,7 +24,7 @@ internal class Program
 
         CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
-        RentalService rentalService = new RentalService (hour, day);
+        RentalService rentalService = new RentalService(hour, day, new BrazilTaxService());
 
         rentalService.ProcessInvoice(carRental);
 
