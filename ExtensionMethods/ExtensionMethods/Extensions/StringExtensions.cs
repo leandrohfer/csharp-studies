9d02c48a2129cb048276d995389b69fb207c8178
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-10-19 13:12:35 -0300

    Exemplo de uso de Extension Methods
    Função Especial de C#

---
+++ ExtensionMethods/ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace System
+{
+    static class StringExtensions
+    {
+        public static string Cut(this string thisObj, int number)
+        {
+            if (thisObj.Length <= number)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, number) + "...";
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2021, 11, 16, 8, 10, 45);
+        Console.WriteLine(dt.ElapsedTime() + "\n");
+
+        String s1 = "Good morning dear students!";
+        Console.WriteLine(s1.Cut(15));
+    }
+}
