2485f07fe73f116e62eb56ef36794e5a606fae33
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-10-20 00:30:05 -0300

    Exemplo Inicial de Delegate Simples

---
+++ ExpressoesLambda_Delegates_LINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using ExpressoesLambda_Delegates_LINQ.Services;
+
+internal class Program
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    private static void Main(string[] args)
+    {
+        double a = 10;
+        double b = 12;
+        // BinaryNumericOperation op = CalculationService.Sum;
+        BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+        // double result = op(a, b);
+        double result = op.Invoke(a, b);
+        Console.WriteLine(result);
+    }
+}
